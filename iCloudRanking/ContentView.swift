d7b8f573401e5fdfd0f8c61d1e2fa74c7b183d04
Javier Calatrava <javi.calatrava@gmail.com>
2025-04-24 11:01:59 +0200

    Blank project

---
+++ iCloudRanking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iCloudRanking
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 24/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iCloudRanking/iCloudRankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCloudRankingApp.swift
+//  iCloudRanking
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 24/4/25.
+//
+
+import SwiftUI
+
+@main
+struct iCloudRankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
